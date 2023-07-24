f9bab552513d4168db52d57605ccf95077ab11b8
largefort <106451813+largefort@users.noreply.github.com>
2023-07-24 10:23:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ let craftingLevel = 1;
 let strengthLevel = 1;
 let agilityLevel = 1;
 let intelligenceLevel = 1;
+let archeryLevel = 1;
+let swordsmanshipLevel = 1;
+let magicLevel = 1;
+let fishingLevel = 1;
+let alchemyLevel = 1;
+let cookingLevel = 1;
+let woodcuttingLevel = 1;
+let miningLevel = 1;
+let smithingLevel = 1;
 
 // Upgrades
 let clickingSpeedUpgrade = 1;
@@ -35,6 +44,33 @@ function trainSkill(skill) {
   } else if (skill === 'intelligence') {
     skillLevel = intelligenceLevel;
     progressBarId = 'intelligenceProgress';
+  } else if (skill === 'archery') {
+    skillLevel = archeryLevel;
+    progressBarId = 'archeryProgress';
+  } else if (skill === 'swordsmanship') {
+    skillLevel = swordsmanshipLevel;
+    progressBarId = 'swordsmanshipProgress';
+  } else if (skill === 'magic') {
+    skillLevel = magicLevel;
+    progressBarId = 'magicProgress';
+  } else if (skill === 'fishing') {
+    skillLevel = fishingLevel;
+    progressBarId = 'fishingProgress';
+  } else if (skill === 'alchemy') {
+    skillLevel = alchemyLevel;
+    progressBarId = 'alchemyProgress';
+  } else if (skill === 'cooking') {
+    skillLevel = cookingLevel;
+    progressBarId = 'cookingProgress';
+  } else if (skill === 'woodcutting') {
+    skillLevel = woodcuttingLevel;
+    progressBarId = 'woodcuttingProgress';
+  } else if (skill === 'mining') {
+    skillLevel = miningLevel;
+    progressBarId = 'miningProgress';
+  } else if (skill === 'smithing') {
+    skillLevel = smithingLevel;
+    progressBarId = 'smithingProgress';
   }
 
   xp += skillLevel * 10;
@@ -84,6 +120,15 @@ function resetGame() {
   strengthLevel = 1;
   agilityLevel = 1;
   intelligenceLevel = 1;
+  archeryLevel = 1;
+  swordsmanshipLevel = 1;
+  magicLevel = 1;
+  fishingLevel = 1;
+  alchemyLevel = 1;
+  cookingLevel = 1;
+  woodcuttingLevel = 1;
+  miningLevel = 1;
+  smithingLevel = 1;
   clickingSpeedUpgrade = 1;
 
   document.getElementById('clickingLevel').innerText = `Level: 1`;
@@ -93,6 +138,15 @@ function resetGame() {
   document.getElementById('strengthLevel').innerText = `Level: 1`;
   document.getElementById('agilityLevel').innerText = `Level: 1`;
   document.getElementById('intelligenceLevel').innerText = `Level: 1`;
+  document.getElementById('archeryLevel').innerText = `Level: 1`;
+  document.getElementById('swordsmanshipLevel').innerText = `Level: 1`;
+  document.getElementById('magicLevel').innerText = `Level: 1`;
+  document.getElementById('fishingLevel').innerText = `Level: 1`;
+  document.getElementById('alchemyLevel').innerText = `Level: 1`;
+  document.getElementById('cookingLevel').innerText = `Level: 1`;
+  document.getElementById('woodcuttingLevel').innerText = `Level: 1`;
+  document.getElementById('miningLevel').innerText = `Level: 1`;
+  document.getElementById('smithingLevel').innerText = `Level: 1`;
   document.getElementById('xp').innerText = `0`;
 
   // Reset progress bars
